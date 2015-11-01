2aa3d0e223b0795dc7cbdad6d48dab52bb7c1621
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-11-01 14:34:16 +0000

    Bike adding to different users under the Owner_id property

--- test-cases/test_1/bike_test_1/app/models/bike.rb
+++ test-cases/test_1/bike_test_1/app/models/bike.rb
@@ -1,7 +1,6 @@
 class Bike < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
-  
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id' 
   has_many :bookings, dependent: :destroy
 
