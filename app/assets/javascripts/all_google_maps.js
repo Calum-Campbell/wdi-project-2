f19a4308e04301ccb47dccdb56c448b8ba7b7da3
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-11-05 13:33:24 +0000

    Tag added to map markers

--- app/assets/javascripts/all_google_maps.js
+++ app/assets/javascripts/all_google_maps.js
@@ -5,6 +5,7 @@ $(function(){
         zoom: 13,
         center: {lat:51.5085300, lng:-0.1257400}
       });
+      var infowindow = new google.maps.InfoWindow();
 
       for (i = 0; i < gon.bikes.length; i++){
 
@@ -15,6 +16,12 @@ $(function(){
         map: map,
         title: 'Hello World!'
       });
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(gon.bikes[i].name);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
     }
     }
     
